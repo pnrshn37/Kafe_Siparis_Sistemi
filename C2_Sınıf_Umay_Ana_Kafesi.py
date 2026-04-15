71fc35f0e9adbfa4acdf2f4fc23c7892c2257f40
pınar sahin <pinrshn37@gmail.com>
2026-04-15 23:17:44 +0300

    Add project files.

---
+++ C2_Sınıf_Umay_Ana_Kafesi.py
@@ -0,0 +1,39 @@
+print("*** Umay Ana Kahve Evi'ne Hoş Geldiniz ***")
+
+# Menü
+menu = {
+    "1": {
+        "kategori": "Sıcak İçecekler",
+        "urunler": {
+            "1": ("Kahve", 60),
+            "2": ("Filtre Kahve", 80),
+            "3": ("Çay", 20),
+            "4": ("Bitki Çayı", 50),
+            "5": ("Sıcak Çikolata", 100),
+            "6": ("White Chocolate Mocha", 150),
+            "7": ("Salep", 45)
+        }
+    },
+
+    "2": {
+        "kategori": "Soğuk İçecekler",
+        "urunler": {
+            "1": ("Limonata", 50),
+            "2": ("Karadut Suyu", 60),
+            "3": ("Cool Lime", 70),
+            "4": ("Buzlu Çay", 80),
+            "5": ("Sade Soda", 40),
+            "6": ("Meyveli Soda", 45),
+            "7": ("Gazoz", 60)
+        }
+    },
+
+    "3": {
+        "kategori": "Tatlılar",
+        "urunler": {
+            "1": ("Çikolatalı Pasta", 75),
+            "2": ("Meyveli Pasta", 80),
+            "3": ("Cheesecake", 85)
+        }
+    }
+}
