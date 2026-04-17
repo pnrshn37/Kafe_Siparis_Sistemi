dda7185ea8b2805fd56de12a9686279a1e922f23
pınar sahin <pinrshn37@gmail.com>
2026-04-18 01:19:48 +0300

    menü güncellendi

--- C2_Sınıf_Umay_Ana_Kafesi.py
+++ C2_Sınıf_Umay_Ana_Kafesi.py
@@ -1,3 +1,5 @@
+siparisler = []
+toplam = 0
 print("*** Umay Ana Kahve Evi'ne Hoş Geldiniz ***")
 
 # Menü
@@ -46,3 +48,61 @@ menu = {
             }
         }
 }
+
+while True:# Programın sürekli çalışmasını sağlar # Kullanıcı çıkış yapana kadar menü tekrar tekrar gösterilir
+    print("\n--- ANA MENÜ ---")
+
+    # Menü sözlüğündeki tüm kategorileri dolaşarak ekrana yazdırır 
+    # key = kullanıcının seçeceği numara
+    # value = kategori bilgisi (örn: Kahveler)
+
+    for key, value in menu.items(): 
+        print(f"{key} - {value['kategori']}") 
+    print("5 - Siparişi Bitir")
+    print("0 - Çıkış")
+
+    secim = input("Bir kategori seçiniz: ") # Kullanıcıdan hangi kategoriye girmek istediğini alır
+
+    if secim == "5":
+        print("\n*** Sipariş Özeti ***")
+        if len(siparisler) == 0:
+            print("Henüz sipariş verilmedi. Lütfen sipariş veriniz.")
+        else:
+            # Kullanıcının seçtiği tüm ürünleri listeler
+            for urun in siparisler:
+                print(f"{urun[0]} - {urun[1]} TL")
+            print(f"Toplam Tutar: {toplam} TL")
+
+    elif secim == "0":  # Kullanıcı 0 girerse programdan çıkar
+        print("Programdan çıkılıyor...")
+        break
+
+    elif secim in menu: # Kullanıcının seçtiği değer menüde var mı kontrol edilir
+        print(f"\n--- {menu[secim]['kategori']} ---")
+
+        alt_menu = menu[secim]["urunler"] # Seçilen kategoriye ait ürünleri alır
+
+        # Seçilen kategorideki ürünleri tek tek ekrana yazdırır
+        for key, value in alt_menu.items(): 
+            print(f"{key} - {value[0]} : {value[1]} TL")
+
+        urun_secim = input("Bir ürün seçiniz: ") # Kullanıcıdan ürün seçmesini ister
+
+        if urun_secim in alt_menu:
+            # Seçilen ürünün adı ve fiyatı alınır
+            urun_adi = alt_menu[urun_secim][0] 
+            fiyat = alt_menu[urun_secim][1]
+
+            # Ürün sipariş listesine eklenir
+            siparisler.append((urun_adi, fiyat))
+            # Toplam tutar güncellenir
+            toplam += fiyat
+
+            print(f"{urun_adi} siparişe eklendi.")
+            # Tüm siparişlerin toplam fiyatını gösterir
+            print(f"Güncel Toplam: {toplam} TL")
+        else:
+            print("Geçersiz ürün seçimi yaptınız.")
+
+    else:
+        print("Geçersiz seçim yaptınız.")
